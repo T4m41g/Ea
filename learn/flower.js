e059c4f3076f14531892b612599ad548f3511150
T4m41g <tamangkta742@gmail.com>
2024-05-31 19:19:01 +0545

    Delete learn directory

--- learn/flower.js
+++
@@ -1,9 +0,0 @@
-// https://freecodez.com
-
-onload = () => {
-    const c = setTimeout(() => {
-      document.getElementById("body").classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
-  
